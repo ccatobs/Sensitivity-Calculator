a2a2b4dcbda813cf14a0ec4df33414929948a780
Ari Mirsky <arimirsky@gmail.com>
2022-06-10 14:06:39 -0400

    Plots

--- data/plotter.py
+++ data/plotter.py
@@ -1,47 +1,46 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-angle = 45
-percentile = 25
-file = open("ACT_annual_" + str(percentile) + "." + str(angle) + ".out", "r")
-data = file.readlines()
 
-x = [float(i.split(" ")[0]) for i in data]
-y = [float(i.split(" ")[1]) for i in data]
+def plotPercentile(percentile):
+    angle = 45
+    file = open("data/ACT_annual_" + str(percentile) +
+                "." + str(angle) + ".out", "r")
+    data = file.readlines()
 
-file.close()
+    x = [float(i.split(" ")[0]) for i in data]
+    y = [float(i.split(" ")[1]) for i in data]
 
-plt.plot(x, y, linewidth=1)
+    file.close()
 
-
-angle = 45
-percentile = 50
-file = open("ACT_annual_" + str(percentile) + "." + str(angle) + ".out", "r")
-data = file.readlines()
-
-x = [float(i.split(" ")[0]) for i in data]
-y = [float(i.split(" ")[1]) for i in data]
-
-file.close()
-
-plt.plot(x, y, linewidth=1)
+    plt.plot(x, y, linewidth=1, label=(str(percentile) + "th percentile"))
 
 
-angle = 45
-percentile = 75
-file = open("ACT_annual_" + str(percentile) + "." + str(angle) + ".out", "r")
-data = file.readlines()
+def plotApproximate(percentile):
+    angle = 45
+    file = open("data/ACT_annual_" + str(percentile) +
+                "_approximation." + str(angle) + ".out", "r")
+    data = file.readlines()
 
-x = [float(i.split(" ")[0]) for i in data]
-y = [float(i.split(" ")[1]) for i in data]
+    x = [float(i.split(" ")[0]) for i in data]
+    y = [float(i.split(" ")[1]) for i in data]
 
-file.close()
+    file.close()
 
-plt.plot(x, y, linewidth=1)
+    plt.plot(x, y, linewidth=1, label=(str(percentile) + "th percentile"))
+
+
+plotPercentile(25)
+# plotApproximate(25)
+plotPercentile(50)
+# plotApproximate(75)
+plotPercentile(75)
+
 plt.ylim(ymin=0, ymax=1)
 plt.xlim(xmin=0, xmax=1000)
 plt.grid(which="both", axis="y")
-plt.title("Transmission vs Frequency")
+plt.legend(loc="upper right")
+plt.title("Transmission vs Frequency 3rd Quartile")
 plt.xlabel("Frequency (GHz)")
 plt.ylabel("Transmission")
 plt.show()
