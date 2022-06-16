1f16a45130e4ad1f410dbd30c907a9281975bbf2
Ari Mirsky <arimirsky@gmail.com>
2022-06-16 09:19:45 -0400

    Removed seasons data

--- data/plotter.py
+++ data/plotter.py
@@ -6,15 +6,9 @@ import numpy as np
 # 3rd Quartile: 2.16470588 = 1.84 mm / 0.85 mm
 
 
-def plotPercentile(percentile, angle, label, season):
-    filePath = None
-    if season == None:
-        filePath = "data/" + str(percentile) + "/ACT_annual_" + \
-            str(percentile) + "." + str(angle) + ".out"
-    else:
-        filePath = "data/" + season + "/" + \
-            str(percentile) + "/ACT_" + season + "_" + \
-            str(percentile) + "." + str(angle) + ".out"
+def plotPercentile(percentile, angle, label):
+    filePath = "data/" + str(percentile) + "/ACT_annual_" + \
+        str(percentile) + "." + str(angle) + ".out"
     file = open(filePath, "r")
     data = file.readlines()
 
@@ -26,22 +20,14 @@ def plotPercentile(percentile, angle, label, season):
     plt.plot(x, y, linewidth=1, label=label)
 
 
-def plotApproximate(percentile, angle, label, season):
+def plotApproximate(percentile, angle, label):
     filePath = None
-    if season == None:
-        if percentile == 50:
-            filePath = "data/50/ACT_annual_" + \
-                str(percentile) + "_approximation." + str(angle) + ".out"
-        else:
-            filePath = "data/" + str(percentile) + "_approx/ACT_annual_" + \
-                str(percentile) + "_approximation." + str(angle) + ".out"
+    if percentile == 50:
+        filePath = "data/50/ACT_annual_" + \
+            str(percentile) + "_approximation." + str(angle) + ".out"
     else:
-        if percentile == 50:
-            filePath = "data/" + season + "/50/ACT_" + \
-                season + "_50." + str(angle) + ".out"
-        else:
-            filePath = "data/" + season + "/" + str(percentile) + "_approx/ACT_" + season + "_" + str(
-                percentile) + "_approximation." + str(angle) + ".out"
+        filePath = "data/" + str(percentile) + "_approx/ACT_annual_" + \
+            str(percentile) + "_approximation." + str(angle) + ".out"
     file = open(filePath, "r")
     data = file.readlines()
 
@@ -52,16 +38,8 @@ def plotApproximate(percentile, angle, label, season):
 
     plt.plot(x, y, linewidth=1, label=label)
 
-
-def parseSeason(season):
-    if season == None:
-        return "Annually"
-    else:
-        return "during season " + season
 
 # Plots MAM at 0 degrees Q1 using version 10.0 and 12.0
-
-
 def plotVersionComparison():
     file = open("data/ACT_MAM_25.0.out", "r")
     data = file.readlines()
@@ -80,70 +58,58 @@ def plotVersionComparison():
 
 
 # Plots the approximation and configuration file for a given percentile and angle
-def plotApproximationAccuracy(percentile, angle, season):
+def plotApproximationAccuracy(percentile, angle):
     if percentile != 25 and percentile != 75:
         print("Configuration files don't exist for that percentile")
         raise SystemExit(1)
     if int(angle) != angle or angle < 0 or angle >= 90:
         print("Interpolation not yet implemented")
         raise SystemExit(1)
-    if season != None and season != "DJF" and season != "JFM" and season != "JJA" and season != "MAM" and season != "SON":
-        print("Season doesn't exist")
-        raise SystemExit(1)
-    plotApproximate(percentile, angle, "Approximation", season)
-    plotPercentile(percentile, angle, "Configuration", season)
+    plotApproximate(percentile, angle, "Approximation")
+    plotPercentile(percentile, angle, "Configuration")
     plt.title("Configuration File Approximation Accuracy " +
               str(percentile) + "th percentile, " + str(angle) + " degrees Zenith Angle")
 
 
 # Plots the transmission at a given angle using configuration files
-def plotPercentiles(angle, season):
+def plotPercentiles(angle):
     if int(angle) != angle or angle < 0 or angle >= 90:
         print("Interpolation not yet implemented")
         raise SystemExit(1)
-    if season != None and season != "DJF" and season != "JFM" and season != "JJA" and season != "MAM" and season != "SON":
-        print("Season doesn't exist")
-        raise SystemExit(1)
-    plotPercentile(25, angle, "25th percentile/1st quartile", season)
-    plotPercentile(50, angle, "50th percentile/2nd quartile", season)
-    plotPercentile(75, angle, "75th percentile/3rd quartile", season)
+    plotPercentile(25, angle, "25th percentile/1st quartile")
+    plotPercentile(50, angle, "50th percentile/2nd quartile")
+    plotPercentile(75, angle, "75th percentile/3rd quartile")
     plt.title("Transmission at Various Atmospheric Conditions at " +
               str(angle) + " degrees Zenith Angle")
 
 
 # Plots the transmission at a given angle using 50th percentile configuration file and an adjusted PWV
-def plotApproximations(angle, season):
+def plotApproximations(angle):
     if int(angle) != angle or angle < 0 or angle >= 90:
         print("Interpolation not yet implemented")
         raise SystemExit(1)
-    if season != None and season != "DJF" and season != "JFM" and season != "JJA" and season != "MAM" and season != "SON":
-        print("Season doesn't exist")
-        raise SystemExit(1)
-    plotApproximate(25, angle, "25th percentile/1st quartile", season)
-    plotPercentile(50, angle, "50th percentile/2nd quartile", season)
-    plotApproximate(75, angle, "75th percentile/3rd quartile", season)
+    plotApproximate(25, angle, "25th percentile/1st quartile")
+    plotPercentile(50, angle, "50th percentile/2nd quartile")
+    plotApproximate(75, angle, "75th percentile/3rd quartile")
     plt.title("Transmission at Various PWV at " +
               str(angle) + " degrees Zenith Angle")
 
 
 # Plots the transmission at a given angle, both the approximations and configuration files
-def plotAll(angle, season):
+def plotAll(angle):
     if int(angle) != angle or angle < 0 or angle >= 90:
         print("Interpolation not yet implemented")
         raise SystemExit(1)
-    if season != None and season != "DJF" and season != "JFM" and season != "JJA" and season != "MAM" and season != "SON":
-        print("Season doesn't exist")
-        raise SystemExit(1)
-    plotApproximate(25, angle, "Q1 Approximation", season)
-    plotPercentile(25, angle, "Q1 Configuration", season)
-    plotPercentile(50, angle, "Q2", season)
-    plotApproximate(75, angle, "Q3 Approximation", season)
-    plotPercentile(75, angle, "Q3 Configuration", season)
+    plotApproximate(25, angle, "Q1 Approximation")
+    plotPercentile(25, angle, "Q1 Configuration")
+    plotPercentile(50, angle, "Q2")
+    plotApproximate(75, angle, "Q3 Approximation")
+    plotPercentile(75, angle, "Q3 Configuration")
     plt.title(
         "Approximations and Configuration Files at " + str(angle) + " degrees Zenith Angle")
 
 
-plotAll(45, None)
+plotAll(45)
 plt.ylim(ymin=0, ymax=1)
 plt.xlim(xmin=0, xmax=1000)
 plt.grid(which="both", axis="y")
