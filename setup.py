08a6bf21a1422c1d20ff631649beb167b34fc784
Ari Mirsky <arimirsky@gmail.com>
2022-11-14 16:53:09 -0500

    Added data files to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(package_data={'sensitivity_calculator': ['data/*', 'input.yaml']})
+    setup(package_data={'sensitivity_calculator': [
+          'data/ACTPWV/ACT_annual_50.45.err', 'data/VariablePWV/*.out', 'data/*.txt', 'input.yaml']})
