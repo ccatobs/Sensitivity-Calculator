3a7ed473b71ce80c319f31c716c2c7c4c409baf4
Ari Mirsky <arimirsky@gmail.com>
2023-05-01 10:20:04 -0500

    Changed to equatorial coordinates and added seeded randomness

--- src/sensitivity_calculator/sensitivity.py
+++ src/sensitivity_calculator/sensitivity.py
@@ -1044,12 +1044,12 @@ def spillEfficiencyComparison(lyotStopAngle=13.4, f=350e9, ds=2.75, maxangle=180
     plt.show()
     plt.clf()
 
-def mapsimsstuffs(i, outputs, noiseCurves): 
+def ccat_mapsims(i, outputs, noiseCurves, channels, pysm_components, seed): 
     instrument_path = Path("/home/amm487/cloned_repos/Sensitivity-Calculator/src/sensitivity_calculator/data/instrument_parameters/instrument_parameters.tbl")
     hitmap_path = "/home/amm487/cloned_repos/Sensitivity-Calculator/src/sensitivity_calculator/data/ccat_uniform_coverage_nside256_201021.fits"
     NSIDE = 256
     cmb = mapsims.SOPrecomputedCMB(
-        num=0,
+        num=seed,
         nside=NSIDE,
         lensed=False,
         aberrated=False,
@@ -1100,7 +1100,7 @@ def mapsimsstuffs(i, outputs, noiseCurves):
         output = tmp*m_apo
         return output
 
-    chs = ["tube:LC1"]
+    chs = channels
     final = []
 
     for ch in chs:
@@ -1108,11 +1108,12 @@ def mapsimsstuffs(i, outputs, noiseCurves):
             channels=ch,
             nside=NSIDE,
             unit="uK_CMB",
-            pysm_output_reference_frame="G",
-            pysm_components_string="a1,c1",
-            pysm_custom_components={"cmb": cmb},
-            other_components={"noise": noise},
-            instrument_parameters=instrument_path
+            pysm_output_reference_frame="C",
+            pysm_components_string=pysm_components,
+            #pysm_custom_components={"cmb": cmb},
+            #other_components={"noise": noise},
+            instrument_parameters=instrument_path,
+            num=seed
         )
         output_map = simulator.execute()
         for det in output_map.keys():
@@ -1127,6 +1128,82 @@ def mapsimsstuffs(i, outputs, noiseCurves):
                 hp.mollview(h[k][pol], title = str(k) + " " + pols[pol])
                 plt.show()
 
+def so_mapsims(i, outputs, noiseCurves, channels, pysm_components, seed): 
+    NSIDE = 256
+    cmb = mapsims.SOPrecomputedCMB(
+        num=seed,
+        nside=NSIDE,
+        lensed=False,
+        aberrated=False,
+        has_polarization=True,
+        cmb_set=0,
+        cmb_dir="data/mapsimscmb",
+        input_units="uK_CMB",
+    )
+    noise = mapsims.SONoiseSimulator(
+        nside=NSIDE,
+        return_uK_CMB=True,
+        sensitivity_mode="baseline",
+        apply_beam_correction=True,
+        apply_kludge_correction=True,
+    )
+    def smooth_map(m, n_it = 5, width=0.1):
+        """Helper to get_window"""
+        i = 0
+        m_apo = m
+        while i<= n_it:
+            m_apo[m_apo<0.8] =0
+            m_apo = hp.smoothing(m_apo, fwhm = width)
+            m_apo[m_apo<0] = 0
+            m_apo /= np.max(m_apo)
+            i+=1
+        return m_apo
+    def get_window(mapk,n_it=5,width = 0.1):
+        """Helper to apodize_map"""
+        m_apo = np.copy(mapk)*0
+        if hp.UNSEEN in np.copy(mapk):
+            m_apo[np.copy(mapk)!=hp.UNSEEN] = 1.0
+        else:
+            m_apo[np.copy(mapk)!=0] = 1.0
+
+        return smooth_map(m_apo, n_it=5, width = 0.1)
+    def apodize_map(map0,n_it =5):
+        """Apodizes a map with hp.UNSEEN pixels as masks. n_it represents the iterations of gaussian convolutions. Higher n_it generally means bigger windows and lower n_it the inverse."""
+
+        tmp = np.copy(map0)
+        tmp2 = np.copy(map0)
+        tmp1 = tmp!=hp.UNSEEN
+        m_apo = get_window(tmp2, n_it = n_it)
+        tmp[tmp1!=True] =0
+        output = tmp*m_apo
+        return output
+
+    chs = channels
+    final = []
+
+    for ch in chs:
+        simulator = mapsims.MapSim(
+            channels=ch,
+            nside=NSIDE,
+            unit="uK_CMB",
+            pysm_output_reference_frame="C",
+            pysm_components_string=pysm_components,
+            #pysm_custom_components={"cmb": cmb},
+            #other_components={"noise": noise},
+            num=seed,
+        )
+        output_map = simulator.execute()
+        for det in output_map.keys():
+            for pol in np.arange(output_map[det].shape[0]):
+                output_map[det][pol] = apodize_map(output_map[det][pol])
+        final.append(output_map)
+    pols = ["T", "Q", "U"]
+    for h in final:
+        for k in h.keys():
+            print(k)
+            for pol in np.arange(h[k].shape[0]):
+                hp.mollview(h[k][pol], title = str(k) + " " + pols[pol])
+                plt.show()
 def _main():
     i = getInputs(os.path.join(
         absolute_path, "input.yaml"))
@@ -1152,7 +1229,9 @@ def _main():
     # outputLoadings(i, calculate)
 
     noiseCurves = getNoiseCurves(i, outputs)
-    mapsimsstuffs(i, outputs, noiseCurves)
+    pysm_components = "d1"
+    ccat_mapsims(i, outputs, noiseCurves, ["tube:LC1"], pysm_components, 0)
+    so_mapsims(i, outputs, noiseCurves, ["tube:LT0"], pysm_components, 0)
     inputs = {'diameter': 5.7, 't': 273, 'wfe': 10.7, 'eta': 0.98, 'doe': 0.8, 'pixelYield': 0.8,
               'eorSpecNumPoln': 2, 't_filter_cold': np.array([1, 1]), 't_lens_cold': np.array([.98, .98]), 't_uhdpe_window': np.array([1, 1]), 'spatialPixels': np.array([3456, 3072]),
               'centerFrequency': np.array([262.5*10**9, 367.5*10**9]), 'detectorNEP': 0,
